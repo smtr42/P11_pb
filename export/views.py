e72c7ee6aaf22c99a370702a2d9d0938a5fc906b
Teiva <t.simonnet@mf2i.fr>
2020-07-22 17:37:40 +0200

    Add template and view

--- export/views.py
+++ export/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from products.managers import ProductManager
+from django.http import HttpResponse
 
 # Create your views here.
 def export(request):
     """Export favorites into a file."""
     ProductManager.get_fav()
-    
-    pass
+
+    response = HttpResponse(content_type="text/csv")
+    fields = [
+        "id",
+        "product",
+        "substitute",
+    ]
+    csv_data = ProductManager.get_fav()
+    csv_data.insert(0, [field.verbose_name for field in Intervention._meta.fields])
+    template = loader.get_template("export/csv.txt")
+    context = {"data": csv_data}
+    response.write(template.render(context))
+
+    return response
+
